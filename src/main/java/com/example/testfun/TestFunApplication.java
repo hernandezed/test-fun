99a896bb03a054a84832170cc70ad6c2163ca4c0
eduardohernandez <hernandezed.1991@gmail.com>
2022-08-24 10:47:54 -0300

    testing

---
+++ src/main/java/com/example/testfun/TestFunApplication.java
@@ -0,0 +1,20 @@
+package com.example.testfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class TestFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestFunApplication.class, args);
+    }
+
+    @Bean
+    public Function<String, String> reverseString() {
+        return value -> new StringBuilder(value).reverse().toString();
+    }
+}
---
+++ src/test/java/com/example/testfun/TestFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
